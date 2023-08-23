33bab30731059e516ec0318463add39ccb70f0bc
allisonabinger <allisonabinger@gmail.com>
2023-08-22 21:54:07 -0700

    fixed indentation in 8

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -7,7 +7,7 @@
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
-		return;
+			return;
 
 		binary_tree_inorder(tree->left, func);
 		binary_tree_inorder(tree->right, func);
