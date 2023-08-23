abd9cf0cd6352f7febf9b8b234bb9ca5d5733677
allisonabinger <allisonabinger@gmail.com>
2023-08-22 19:26:56 -0700

    added 4

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+	* binary_tree_is_leaf - checks to see if a node is a leaf
+	* @node: node to check
+	* Return: 1 if leaf, 0 otherwise
+	*/
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	
+	if (node-> == NULL && node->right == NULL)
+		return (1);
+	
+	return (0);./gi	
+}
