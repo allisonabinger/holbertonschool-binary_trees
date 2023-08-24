bcecfa48a6668c9401b45d0e52386d0b8cd3bee1
allisonabinger <allisonabinger@gmail.com>
2023-08-24 11:30:14 -0700

    fixed 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -2,15 +2,16 @@
 /**
 	* binary_tree_balance - measures balance factor of binary tree
 	* @tree: pointer to root node of tree
-	* Return: 
+	* Return:
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (tree == NULL)
+	{
 		return (0);
-	
+	}
 	int left_height = (int)binary_tree_height(tree->left);
-	int right_heigh = (int)binary_tree_height(tree->right);
+	int right_height = (int)binary_tree_height(tree->right);
 
 	return (left_height - right_height);
 }
