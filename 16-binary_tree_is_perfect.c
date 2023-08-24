bd560e611abb507d875101061a684e9e455554aa
allisonabinger <allisonabinger@gmail.com>
2023-08-24 12:29:06 -0700

    added is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,25 @@
 #include "binary_trees.h"
+#include "10-binary_tree_depth.c"
 /**
-	* 
-	* 
-	*
+	* binary_tree_is_perfect - checks if a binary tree is perfect
+	* @tree: pointer to root node
+	* Return: 1 if perfect, 0 if not
 */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int dl, dr;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	dl = binary_tree_depth(tree->left);
+	dr = binary_tree_depth(tree->right);
+
+	if (dl == dr)
+	{
+		return (1);
+	}
+
+	return (0);
+}
