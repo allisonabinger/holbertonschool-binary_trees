4a2f04f8c705593175bbc0f42fd977b84be0db86
allisonabinger <allisonabinger@gmail.com>
2023-08-24 10:21:01 -0700

    added betty docs

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 /**
-	*
-	*
-	*
+	* binary_tree_leaves - counts leaves in a tree
+	* @tree: root node
+	* Return: leaves or 0 if NULL
 */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
